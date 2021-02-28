ed702a4442d2434b4435a3a7b3f8659112b0f5fc
SapuSeven <sapuseven@gmail.com>
2021-02-28 03:18:50 +0100

    Create TwitchChatFilter.meta.js

---
+++ TwitchChatFilter.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Twitch Chat Filter
+// @namespace    https://github.com/SapuSeven/TwitchChatFilter
+// @version      1.0
+// @description  Making the Twitch.tv Chat more enjoyable. Configure using the Twitch chat settings.
+// @author       SapuSeven
+// @updateURL   https://github.com/SapuSeven/TwitchChatFilter/raw/main/TwitchChatFilter.meta.js
+// @downloadURL https://github.com/SapuSeven/TwitchChatFilter/raw/main/TwitchChatFilter.user.js
+// @include     /^https?://(www\.)?twitch\.tv(\/.*)?$/
+// @grant       none
+// @run-at      document-end
+// ==/UserScript==
