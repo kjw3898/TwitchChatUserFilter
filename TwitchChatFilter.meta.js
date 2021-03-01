900693c45cbd287042b5e96b988b369163053ef1
kjw3898 <kjw3898@gmail.com>
2021-03-01 15:25:04 +0900

    Update TwitchChatFilter.meta.js

--- TwitchChatFilter.meta.js
+++ TwitchChatFilter.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         Twitch Chat Filter
-// @namespace    https://github.com/SapuSeven/TwitchChatFilter
+// @name         Twitch Chat User Filter
+// @namespace    https://github.com/kjw3898/TwitchChatUserFilter/
 // @version      1.0
-// @description  Making the Twitch.tv Chat more enjoyable. Configure using the Twitch chat settings.
-// @author       SapuSeven
-// @updateURL   https://github.com/SapuSeven/TwitchChatFilter/raw/main/TwitchChatFilter.meta.js
-// @downloadURL https://github.com/SapuSeven/TwitchChatFilter/raw/main/TwitchChatFilter.user.js
+// @description  twitch chat filter by username (whitelist, blacklist)
+// @author       kjw3898
+// @updateURL   https://github.com/kjw3898/TwitchChatUserFilter/raw/main/TwitchChatFilter.meta.js
+// @downloadURL https://github.com/kjw3898/TwitchChatUserFilter/raw/main/TwitchChatFilter.user.js
 // @include     /^https?://(www\.)?twitch\.tv(\/.*)?$/
 // @grant       none
 // @run-at      document-end
